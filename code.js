ede627f0b743550accf9572fb08373edc44a4508
Dreacel <143748787+Dreacel@users.noreply.github.com>
2026-02-20 22:51:16 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,9 @@
 const animals = ["Rat","Ox","Tiger","Rabbit","Dragon","Snake","Goat","Monkey","Rooster","Dog","Pig"];
 
 const text = document.querySelector(".title");
-const changeColor = document.querySelector(".newcolor");
+const changeColor = document.querySelector(".newColor");
 
-changeColor.addEventListener('click', function () {
-    text.classList.toggle("change")
-});
+
 
 for (animal of animals) {
     console.log("your animal is a " + animal);
