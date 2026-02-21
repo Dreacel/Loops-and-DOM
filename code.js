abd305d8859a9e8f6041b52579cba10fe1f3c8d4
Dreacel <143748787+Dreacel@users.noreply.github.com>
2026-02-20 21:24:04 -0600

    loop

---
+++ code.js
@@ -0,0 +1,5 @@
+const animals = ["Rat","Ox,","Tiger","Rabbit","Dragon","Snake","Goat","Monkey","Rooster","Dog","Pig"];
+
+for (animal of animals) {
+    console.log('your animal is a ${animal}');
+}
